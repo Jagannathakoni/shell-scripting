ffe073b47233dfba19836ca343bee6dcfc8e010f
Jagannathakoni <jagannatha.koni@gmail.com>
2024-01-25 06:50:17 +0000

    new commiting

---
+++ sample.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "print storage details of mahine"
+echo "______________"
+echo "df -h"
+echo "print memory details of machine"
+echo "free -g"
+echo "______________"
+echo "print generic details of machine"
+echo "who && uname -r"
+echo "______________"
---
+++ server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo -n "my name"
+read severname
+echo "myname: $servername"
+echo -n "my operating system"
+read OS
+echo "my operating system: $OS"
+
