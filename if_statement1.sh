08243a61814c2bba20477e221485665eda83ccb5
Ubuntu <ubuntu@ip-172-31-5-126.ap-south-1.compute.internal>
2024-12-16 08:03:02 +0000

    added if_statement file

---
+++ if_statement1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Jagannatha K"
+read name
+echo "83"
+read marks
+
+if [ $marks is -ge 50 ]
+then
+	echo "$name, your are eligible"
+
+else
+	echo "$name, you are not eligible"
+fi
